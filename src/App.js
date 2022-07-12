56baffef05d551dffb5530912b76f615089c06b1
JohelPires <johelpires@gmail.com>
2022-07-12 14:07:00 -0400

    routes and page components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { Layout } from 'antd'
+import { Routes, Route, Link } from 'react-router-dom'
 import './App.scss'
+import Crypto from './components/Crypto'
+import Exchange from './components/Exchange'
+import Homepage from './components/Homepage'
 import Navbar from './components/Navbar'
+import News from './components/News'
 
 function App() {
   return (
@@ -8,8 +13,19 @@ function App() {
       <div className='navbar'>
         <Navbar />
       </div>
-      <div className='main'>main</div>
-      <div className='footer'>footer</div>
+      <div className='main'>
+        <Layout>
+          <div className='routes'>
+            <Routes>
+              <Route exact path='/' element={<Homepage />} />
+              <Route exact path='/crypto' element={<Crypto />} />
+              <Route exact path='/exchange' element={<Exchange />} />
+              <Route exact path='/news' element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+      </div>
+      {/* <div className='footer'>footer</div> */}
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,10 @@
-import { FundOutlined, HomeOutlined, MenuOutlined } from '@ant-design/icons'
+import {
+  BulbOutlined,
+  FundOutlined,
+  HomeOutlined,
+  MenuOutlined,
+  MoneyCollectOutlined,
+} from '@ant-design/icons'
 import { Avatar, Button, Menu, Typography } from 'antd'
 import React from 'react'
 import { Link } from 'react-router-dom'
@@ -14,21 +20,18 @@ function Navbar() {
         </Typography.Title>
         {/* <Button className='menu-control-container'></Button> */}
       </div>
-      <Menu theme='dark'>
+      <Menu>
         <Menu.Item key='home' icon={<HomeOutlined />}>
           <Link to='/'>Início</Link>
         </Menu.Item>
         <Menu.Item key='crypto' icon={<FundOutlined />}>
           <Link to='/crypto'>Crypto Moedas</Link>
         </Menu.Item>
-        <Menu.Item key='home' icon={<HomeOutlined />}>
-          <Link to='/'>Início</Link>
+        <Menu.Item key='exchange' icon={<MoneyCollectOutlined />}>
+          <Link to='/exchange'>Operadoras</Link>
         </Menu.Item>
-        <Menu.Item key='home' icon={<HomeOutlined />}>
-          <Link to='/'>Início</Link>
-        </Menu.Item>
-        <Menu.Item key='home' icon={<HomeOutlined />}>
-          <Link to='/'>Início</Link>
+        <Menu.Item key='news' icon={<BulbOutlined />}>
+          <Link to='/news'>Notícias</Link>
         </Menu.Item>
       </Menu>
     </div>
