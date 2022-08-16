764058dfe0b18fa2bb56306ba9ab01a2384a8c19
JohelPires <johelpires@gmail.com>
2022-08-16 11:31:37 -0400

    details stats

--- src/components/Details.js
+++ src/components/Details.js
@@ -23,72 +23,73 @@ function Details() {
   const [timePeriod, setTimePeriod] = useState('7d')
   const time = ['3h', '24h', '7d', '30d', '3m', '1y', '3y', '5y']
 
-  //   const cd = dados?.data?.coin
+  const cd = data?.data?.coin
 
-  //   const stats = [
-  //     {
-  //       title: 'Preço em USD',
-  //       value: `$ ${cd?.price && millify(cd?.price)}`,
-  //       icon: <DollarCircleOutlined />,
-  //     },
-  //     {
-  //       title: 'Rank',
-  //       value: `$ ${cd?.rank}`,
-  //       icon: <NumberOutlined />,
-  //     },
-  //     {
-  //       title: '24h Volume',
-  //       value: `$ ${cd?.volume && millify(cd?.volume)}`,
-  //       icon: <ThunderboltOutlined />,
-  //     },
-  //     {
-  //       title: 'Valor de Mercado',
-  //       value: `$ ${cd?.marketCap && millify(cd?.marketCap)}`,
-  //       icon: <DollarCircleOutlined />,
-  //     },
-  //     {
-  //       title: 'Alta histórica',
-  //       value: `$ ${millify(cd?.allTimeHigh?.price)}`,
-  //       icon: <TrophyOutlined />,
-  //     },
-  //   ]
-  //   const genericStats = [
-  //     {
-  //       title: 'Quantidade de Mercados',
-  //       value: cd?.numberOfMarkets,
-  //       icon: <FundOutlined />,
-  //     },
-  //     {
-  //       title: 'Número de Operadoras',
-  //       value: cd?.numberOfExchanges,
-  //       icon: <MoneyCollectOutlined />,
-  //     },
-  //     {
-  //       title: 'Approved Supply',
-  //       value: cd?.approvedSupply ? <CheckOutlined /> : <StopOutlined />,
-  //       icon: <ExclamationCircleOutlined />,
-  //     },
-  //     {
-  //       title: 'Total Supply',
-  //       value: cd?.totalSupply,
-  //       icon: <DollarCircleOutlined />,
-  //     },
-  //     {
-  //       title: 'Circulating Supply',
-  //       value: cd?.circulatingSupply,
-  //       icon: <TrophyOutlined />,
-  //     },
-  //   ]
+  const stats = [
+    {
+      title: 'Preço em USD',
+      value: `$ ${cd?.price && millify(cd?.price)}`,
+      icon: <DollarCircleOutlined />,
+    },
+    {
+      title: 'Rank',
+      value: `${cd?.rank}`,
+      icon: <NumberOutlined />,
+    },
+    {
+      title: '24h Volume',
+      value: `${cd['24hVolume'] && millify(cd['24hVolume'])}`,
+      icon: <ThunderboltOutlined />,
+    },
+    {
+      title: 'Valor de Mercado',
+      value: `$ ${cd?.marketCap && millify(cd?.marketCap)}`,
+      icon: <DollarCircleOutlined />,
+    },
+    {
+      title: 'Alta histórica',
+      value: `$ ${millify(cd?.allTimeHigh?.price)}`,
+      icon: <TrophyOutlined />,
+    },
+  ]
+  const genericStats = [
+    {
+      title: 'Quantidade de Mercados',
+      value: cd?.numberOfMarkets,
+      icon: <FundOutlined />,
+    },
+    {
+      title: 'Número de Operadoras',
+      value: cd?.numberOfExchanges,
+      icon: <MoneyCollectOutlined />,
+    },
+    {
+      title: 'Approved Supply',
+      value: cd?.approvedSupply ? <CheckOutlined /> : <StopOutlined />,
+      icon: <ExclamationCircleOutlined />,
+    },
+    {
+      title: 'Total Supply',
+      value: cd?.totalSupply,
+      icon: <DollarCircleOutlined />,
+    },
+    {
+      title: 'Circulating Supply',
+      value: cd?.circulatingSupply,
+      icon: <TrophyOutlined />,
+    },
+  ]
 
   if (isFetching) return 'Carregando...'
   console.log(data)
 
   return (
     <Col className='coin-detail-container'>
-      {/* <Col className='coin-heading-container'>
+      <Col className='coin-heading-container'>
         <Typography.Title level={2} className='coin-name'>
-          {cd?.name} ({cd?.symbol})
+          {data.data?.coin?.name} ({data.data?.coin?.symbol})
         </Typography.Title>
+        {/* <p>{data.data?.coin?.description}</p> */}
       </Col>
       <Select
         defaultValue='7d'
@@ -99,7 +100,26 @@ function Details() {
         {time.map((p) => (
           <Option key={p}>{p}</Option>
         ))}
-      </Select> */}
+      </Select>
+      <p>line chart for {timePeriod}</p>
+      <Col className='stats-container'>
+        <Col className='coin-value-statistics'>
+          <Col className='coin-value-statistics-heading'>
+            <Typography.Title className='coin-details-heading' level={3}>
+              Estatísticas para o {data.data?.coin?.name}
+            </Typography.Title>
+          </Col>
+          {stats.map(({ icon, title, value }) => (
+            <Col className='coin-stats'>
+              <Col className='coin-stats-name'>
+                <Typography.Text>{icon}</Typography.Text>
+                <Typography.Text>{title}</Typography.Text>
+              </Col>
+              <Typography.Text className='stats'>{value}</Typography.Text>
+            </Col>
+          ))}
+        </Col>
+      </Col>
     </Col>
   )
 }
