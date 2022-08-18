401f45be2ec7694e723662200774866390f807a1
JohelPires <johelpires@gmail.com>
2022-08-18 10:46:59 -0400

    missing the exchange screen

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,9 +7,34 @@ import {
 } from '@ant-design/icons'
 import { Avatar, Button, Menu, Typography } from 'antd'
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function Navbar() {
+  const [activeMenu, setActiveMenu] = useState(true)
+  const [screenSize, setScreenSize] = useState(null)
+
+  useEffect(() => {
+    function handleResize() {
+      setScreenSize(window.innerWidth)
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    handleResize()
+
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  useEffect(() => {
+    if (screenSize < 768) {
+      setActiveMenu(false)
+    } else {
+      setActiveMenu(true)
+    }
+  }, [screenSize])
+
   return (
     <div className='nav-container'>
       <div className='logo-container'>
@@ -20,20 +45,22 @@ function Navbar() {
         </Typography.Title>
         {/* <Button className='menu-control-container'></Button> */}
       </div>
-      <Menu theme='dark'>
-        <Menu.Item key='home' icon={<HomeOutlined />}>
-          <Link to='/'>Início</Link>
-        </Menu.Item>
-        <Menu.Item key='crypto' icon={<FundOutlined />}>
-          <Link to='/crypto'>Crypto Moedas</Link>
-        </Menu.Item>
-        <Menu.Item key='exchange' icon={<MoneyCollectOutlined />}>
-          <Link to='/exchange'>Operadoras</Link>
-        </Menu.Item>
-        <Menu.Item key='news' icon={<BulbOutlined />}>
-          <Link to='/news'>Notícias</Link>
-        </Menu.Item>
-      </Menu>
+      {activeMenu && (
+        <Menu theme='dark'>
+          <Menu.Item key='home' icon={<HomeOutlined />}>
+            <Link to='/'>Início</Link>
+          </Menu.Item>
+          <Menu.Item key='crypto' icon={<FundOutlined />}>
+            <Link to='/crypto'>Crypto Moedas</Link>
+          </Menu.Item>
+          <Menu.Item key='exchange' icon={<MoneyCollectOutlined />}>
+            <Link to='/exchange'>Operadoras</Link>
+          </Menu.Item>
+          <Menu.Item key='news' icon={<BulbOutlined />}>
+            <Link to='/news'>Notícias</Link>
+          </Menu.Item>
+        </Menu>
+      )}
     </div>
   )
 }
